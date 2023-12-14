b2eb9f7112832824a8be82914a213f700eb7be48
WebShop <muhamadafghanalzena@gmail.com>
2023-12-14 08:51:15 +0700

    protect

--- script.js
+++ script.js
@@ -28,4 +28,42 @@ function hideNav() {
   btnNavMenu.style.display = "inherit";
   btnNavHidden.style.display = "none";
   hiddenNav.classList.add("hidden");
-}window.addEventListener("resize",function(){if(window.outerWidth-window.innerWidth>100||window.outerHeight-window.innerHeight>100){window.location.href = "https://bit.ly/jsbveigbwpf";}});
+}
+window.addEventListener("resize", function () {
+  if (
+    window.outerWidth - window.innerWidth > 100 ||
+    window.outerHeight - window.innerHeight > 100
+  ) {
+    resetPage()
+  }
+});
+
+// function resetPage() {
+//   document.body.innerHTML = "ERROR_NOT_FOUND  <a href='https://bit.ly/portofolio-kangAL'><button style='color: black; padding: 10px 20px; font-size: 20px;'>kembali</button></a>";
+// }
+
+document.addEventListener("keydown", function (event) {
+  if (
+    (event.ctrlKey && event.key === "u") ||
+    event.key === "F12" ||
+    (event.ctrlKey && event.shiftKey && event.key === "I")
+  ) {
+    resetPage();
+  }
+});
+
+    function blockConsole(event) {
+      if (event.ctrlKey && event.shiftKey && (event.key === 'I' || event.key === 'C' || event.key === 'J' || event.key === 'U')) {
+        event.preventDefault();
+      } else if (event.ctrlKey && event.key === 'u') {
+        event.preventDefault();
+      } else if (event.key === 'F12') {
+        event.preventDefault();
+      }
+    }
+
+    window.addEventListener('keydown', blockConsole);
+
+    window.addEventListener('contextmenu', function (event) {
+      event.preventDefault();
+    });
